c33fb8a6fd1d2f739d2c8cfea2bfd05315291e79
Johan Thoren <johan@thoren.xyz>
2021-09-13 08:49:28 +0200

    1.1.0-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject xyz.thoren/equinox "1.0.1-SNAPSHOT"
+(defproject xyz.thoren/equinox "1.1.0-SNAPSHOT"
   :description "Calculate equinoxes and solstices for a given year"
   :url "https://github.com/johanthoren/equinox"
   :license {:name "ISC"
